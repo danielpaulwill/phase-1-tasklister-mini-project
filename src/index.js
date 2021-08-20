21f1c62a77d1ccf55304d7a90ca7adc23f03d257
Daniel Williams <danielpaulwill@gmail.com>
2021-08-19 22:16:01 -0400

    Some stretch deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addToDo(e.target.querySelector('#new-task-description').value)
+    console.log(document.querySelector('#priority').value)
+  })
+
+function addToDo(todo) {
+  let p = document.createElement("p")
+  let list = document.querySelector('#list')
+  p.textContent = todo;
+  let btn = document.createElement('button')
+  btn.addEventListener('click', removeToDo)
+  btn.textContent = "X"
+  let priorityValue = document.querySelector('#priority').value
+  if (priorityValue === 'high') {
+    p.style="color:red"
+  }
+  if (priorityValue === 'medium') {
+    p.style="color:yellow"
+  }
+  if (priorityValue === 'low') {
+    p.style="color:green"
+  }
+  p.appendChild(btn)
+  list.appendChild(p)
+}
+
+function removeToDo(e) {
+  e.target.parentNode.remove();
+  };
+
 });
